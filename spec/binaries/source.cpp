9c83ce97e1619887cb4d651b543684312d938aa0
david942j <david942j@gmail.com>
2017-10-04 23:40:57 +0800

    fix tests

--- spec/binaries/source.cpp
+++ spec/binaries/source.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main() {
+int main(int argc, char **argv) {
+  if(argc > 1) {
+    for(int i=1;i<argc;i++)
+      puts(argv[i]);
+    return 0;
+  }
   int n;
   while(~scanf("%d", &n) && n)
     printf("%d\n", n);
--- spec/gdb_spec.rb
+++ spec/gdb_spec.rb
@@ -14,11 +14,11 @@ describe GDB::GDB do
 
   it 'initialize' do
     @new_gdb.call('amd64.elf') do |gdb|
-      expect(gdb.execute('break main')).to eq 'Breakpoint 1 at 0x4005da'
+      expect(gdb.execute('break main')).to eq 'Breakpoint 1 at 0x40062a'
     end
 
     @new_gdb.call('amd64.pie.elf', args: '-nh') do |gdb|
-      expect(gdb.execute('break main')).to eq 'Breakpoint 1 at 0x814'
+      expect(gdb.execute('break main')).to eq 'Breakpoint 1 at 0x854'
       expect(gdb.execute('run').lines.first.strip).to eq <<-EOS.strip
 Starting program: #{@binpath['amd64.pie.elf']}
       EOS
@@ -31,8 +31,8 @@ Starting program: #{@binpath['amd64.pie.elf']}
 
   it 'break' do
     @new_gdb.call('amd64.elf') do |gdb|
-      expect(gdb.break('main')).to eq 'Breakpoint 1 at 0x4005da'
-      expect(gdb.b(0x4005da)).to eq "Note: breakpoint 1 also set at pc 0x4005da.\r\nBreakpoint 2 at 0x4005da"
+      expect(gdb.break('main')).to eq 'Breakpoint 1 at 0x40062a'
+      expect(gdb.b(0x40062a)).to eq "Note: breakpoint 1 also set at pc 0x40062a.\r\nBreakpoint 2 at 0x40062a"
     end
   end
 
@@ -46,8 +46,8 @@ Starting program: #{@binpath['amd64.pie.elf']}
   end
 
   it 'run' do
-    @new_gdb.call('zsh', args: '-nh') do |gdb|
-      expect(gdb.run('-c "echo 1111"').lines[1].strip).to eq '1111'
+    @new_gdb.call('amd64.elf', args: '-nh') do |gdb|
+      expect(gdb.run('1111').lines[1].strip).to eq '1111'
     end
   end
 
@@ -57,8 +57,8 @@ Starting program: #{@binpath['amd64.pie.elf']}
       gdb.b('main')
       gdb.run
       expect(gdb.register(:rdi)).to be 1
-      expect(gdb.register(:rax)).to be 0x4005d6
-      expect(gdb.register(:al)).to be 0xd6
+      expect(gdb.register(:rax)).to be 0x400626
+      expect(gdb.register(:ah)).to be 0x6
     end
   end
 
@@ -83,16 +83,17 @@ Starting program: #{@binpath['amd64.pie.elf']}
   end
 
   it 'write_memory' do
-    @new_gdb.call('zsh', args: '-nh') do |gdb|
+    @new_gdb.call('amd64.elf', args: '-nh') do |gdb|
       gdb.b('main')
-      gdb.r('-c "echo 123"')
+      gdb.r('pusheen "the cat"')
       argv2 = gdb.read_memory(gdb.register(:rsi) + 16, 1, as: :uint64)
-      expect(gdb.read_memory(argv2, 8)).to eq 'echo 123'
-      gdb.write_memory(argv2 + 5, 'ABC')
+      expect(gdb.read_memory(argv2, 7)).to eq 'the cat'
+      gdb.write_memory(argv2 + 4, 'FAT')
       pid = gdb.pid
       expect(gdb.execute('continue').lines.map(&:strip).join("\n")).to eq <<-EOS.strip
 Continuing.
-ABC
+pusheen
+the FAT
 [Inferior 1 (process #{pid}) exited normally]
       EOS
     end
